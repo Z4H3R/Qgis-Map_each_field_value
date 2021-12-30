47a0bdc9a19ac35f5e374daee2ac270b4819c1d7
Z4H3R <96667626+Z4H3R@users.noreply.github.com>
2021-12-30 09:45:13 +0200

    Update Map_by_Cycle_Field_Values - Z4H3R.py

--- Map_by_Cycle_Field_Values - Z4H3R.py
+++ Map_by_Cycle_Field_Values - Z4H3R.py
@@ -1,21 +1,22 @@
-field_name = '"your_field_name"' # - Name of the field you want to cycle.
-field_index =  # - Index of the field you want to cycle. You find it in: Layer properties -> Fields
-layoutName = "your_layout_name" # - Name of the Print Layout you want to use.
-png_path = '/Users/your_user/your_folder/' # - Path where you want to save the .png files.
-png_name_template = "your_file_name{}.png" # - Name of the .png file. Put "{}" where you want the field_value in the file name.
+import os
+field_name = 'Name' # - Name of the field you want to cycle.
+layoutName = "INV MAR - HS - DEC 2021 -  - A5 150DPI" # - Name of the Print Layout you want to use.
+png_path = r'C:\Users\Z4H3R\Desktop\TEST' # - Path where you want to save the .png files.
+png_name_template = "INV MAR - {} - A5 150DPI.png" # - Name of the .png file. Use "{}" where you want the field_value inserted in the file name.
+label_name = layout.itemById("field_value") # - ID of the label in Print Layout you want to show the unique value beeing mapped. You must first set it in the Layout you want to use.
 project = QgsProject.instance()
 layer = iface.activeLayer()
+field_index = layer.fields().indexFromName(field_name)
 values_list = layer.uniqueValues(field_index)
 manager = project.layoutManager()
 layout = manager.layoutByName(layoutName)
 exporter = QgsLayoutExporter(layout)
 for field_value in values_list:
-    subset_str = "'" + str(field_value)+"'"+'LIKE '+ field_name
+    subset_str = "'" + str(field_value)+"'"+'LIKE '+ '"' + field_name + '"'
     layer.setSubsetString(subset_str)
-    pngName = png_name_template.format(field_value) 
-    dest_file = str(png_path + pngName)
-    label_name = layout.itemById("field_value")
+    pngName = png_name_template.format(field_value)
+    dest_file = os.path.join(png_path, pngName)
     label_name.setText(field_value)
     exporter.exportToImage(dest_file, QgsLayoutExporter.ImageExportSettings())
     layer.setSubsetString('')
-print("done")
+print("DONE")
